84e1cc3cbf4fd110aba23f87c0febf3507703947
yaremenko <yaremenko@ultimeta.ru>
2024-01-21 13:05:01 +0400

    Metrics

--- src/main/java/com/jpoint/demoshop/DemoShopApplication.java
+++ src/main/java/com/jpoint/demoshop/DemoShopApplication.java
@@ -1,9 +1,15 @@
 package com.jpoint.demoshop;
 
+import com.codahale.metrics.ConsoleReporter;
+import com.codahale.metrics.MetricAttribute;
+import com.codahale.metrics.MetricRegistry;
 import com.jpoint.demoshop.service.PopulationService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class DemoShopApplication implements CommandLineRunner {
@@ -22,8 +28,38 @@ public class DemoShopApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         long l = System.currentTimeMillis();
-        populationService.populateTables();
+
+        MetricRegistry metricRegistry = new MetricRegistry();
+        var timer = metricRegistry.timer("main");
+
+        var iterationCount = 10;
+        for (int i = 0; i < iterationCount; i++) {
+            try (var ignored = timer.time()) {
+                populationService.populateTables();
+            }
+        }
+
+        try (var reporter = makeReporter(metricRegistry)) {
+            reporter.report();
+        }
+
         System.out.printf("Executed in %d second(s) \n", (System.currentTimeMillis() - l) / 1000);
         System.out.println("OK");
+    }
+
+    private ConsoleReporter makeReporter(MetricRegistry metricRegistry) {
+        return ConsoleReporter.forRegistry(metricRegistry)
+                .convertDurationsTo(TimeUnit.SECONDS)
+                .disabledMetricAttributes(Set.of(
+                        MetricAttribute.M1_RATE,
+                        MetricAttribute.M5_RATE,
+                        MetricAttribute.M15_RATE,
+                        MetricAttribute.P75,
+                        MetricAttribute.P95,
+                        MetricAttribute.P98,
+                        MetricAttribute.P99,
+                        MetricAttribute.P999
+                ))
+                .build();
     }
 }
